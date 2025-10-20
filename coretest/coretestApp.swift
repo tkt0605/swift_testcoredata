c74630b395fd31c3a7ed6861098fc3afb363fd69
tkt0605 <takato.komada2005@outlook.jp>
2025-10-20 13:55:53 +0900

    Initial Commit

---
+++ coretest/coretestApp.swift
@@ -0,0 +1,21 @@
+//
+//  coretestApp.swift
+//  coretest
+//
+//  Created by 駒田隆人 on 2025/10/20.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct coretestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
